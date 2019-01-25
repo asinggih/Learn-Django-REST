4517e0aeb379ee60e0998ec953087144093064bc
Aditya Singgih <adityasinggih@msn.com>
2019-01-25 23:54:38 +1100

    added POST method into our ApiNameHere api

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 
-# Create your views here.
 class ApiNameHere(APIView):
     """
     Test API View.
     This is inside the doctest of the class
     """
 
-    # API View lets us define standard HTTP methods
+    # It tells Django that the serializer class of this API view
+    # is gonna be the ApiNameSerializer class from serializers
+    serializer_class = serializers.ApiNameSerializer
 
+    # API View lets us define standard HTTP methods
     def get(self, request, format=None):
         """Returns a list of APIView features"""
 
@@ -27,3 +32,21 @@ class ApiNameHere(APIView):
             'message': 'Hello',
             'apiview_list': apiview_list
         })
+
+    def post(self, request):
+        """Returns a message with the inserted quote"""
+
+        # serialise the payload of the POST request
+        serializer = serializers.ApiNameSerializer(data=request.data)
+
+        if serializer.is_valid():
+            # the data that we get has to be the something that we defined
+            # inside the serializser of this particular API class
+            quote = serializer.data.get('random_quote')
+            message = "you say {}".format(quote)
+            return Response({
+                "message": message
+            })
+
+        # standardised error response if serializser is not valid
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
